ea49f6d20c58a7186612fbe4b7154bdcaf5e4cd2
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-08 09:31:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sarcasm-App---Beginner-Javascript
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vxqpxm)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Sarcasm for Twitter</h1>`;
